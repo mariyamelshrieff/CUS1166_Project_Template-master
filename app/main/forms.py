f6b92cc03dfdf7d4f991c664531a92edc101a548
mariyamelshrief <mariyam.elshrief17@my.stjohns.edu>
2019-12-05 13:41:58 -0500

    Updates appointments

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,8 @@
 # import flask_wtf
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, TextAreaField, DateTimeField, IntegerField
-from wtforms.validators import ValidationError, DataRequired, Length, NumberRange
+from wtforms import StringField, SubmitField, SelectField, TextAreaField, BooleanField, DateTimeField, IntegerField, FloatField
+from wtforms.validators import ValidationError, DataRequired, Length
+
 
 
 class TaskForm(FlaskForm):
@@ -9,82 +10,15 @@ class TaskForm(FlaskForm):
     task_status_completed = SelectField('Status', choices=[('todo','Todo'),('doing','Doing'),('done','Done')])
     submit = SubmitField('submit')
 
-
-
 class AppointmentForm(FlaskForm):
-    appointment_desc = StringField('appointment_desc', validators=[DataRequired()])
-
-    #appointment_title = StringField('appointment_title', validators=[DataRequired()])
-    #appointment_customername = StringField('appointment_customername', validators=[DataRequired()])
-    #appointment_location = StringField('appointment_location', validators=[DataRequired()])
-    #appointment_date = StringField('appointment_date', validators=[DataRequired()])
-
-    appointment_status_completed = SelectField('Status', choices=[('upcoming','Upcoming'),('in progress','In Progress'),('completed','Completed')])
+    appointment_title = StringField('Appointment Title', validators=[DataRequired()])
+    appointment_start_date = DateTimeField('Start Time', validators=[DataRequired()])
+    appointment_duration = StringField('Duration', validators=[DataRequired()])
+    appointment_location = StringField('Customer Address', validators=[DataRequired()])
+    customer_name = StringField('Customer Name', validators=[DataRequired()])
+    customer_notes = StringField('Customer Notes', validators=[DataRequired()])
     submit = SubmitField('submit')
 
-
-class AppointmentCreationForm(FlaskForm):
-    title = StringField(
-        label='Title',
-        validators=[DataRequired(),
-                    Length(max=255, min=3, message='Title Allow 2-255 chars')],
-
-    )
-    customer = StringField(
-        label='Customer',
-        validators=[DataRequired(),
-                    Length(max=255, min=3, message='Customer Allow 2-255 chars')],)
-    location = StringField(
-        label='Location',
-        validators=[DataRequired(),
-                    Length(max=255, min=3, message='Location Allow 2-255 chars')
-                    ],
-    )
-    start_time = DateTimeField(
-        label='Date and Start Time',
-        validators=[DataRequired()],
-        format='%Y-%m-%d %H:%M'
-    )
-    duration = IntegerField(
-        label='Meeting Duration(is Minutes)',
-        validators=[DataRequired(),
-                    NumberRange(min=10, max=1440, message='Meeting Duration should be Integer and between 10-1440')])
-    note = TextAreaField(label='Notes')
-    submit = SubmitField('submit')
-
-
-is_completed_choices = [('Not Completed', 'Not Completed', ('Completed', 'Completed'))]
-
-class AppointmentEditionForm(FlaskForm):
-    title = StringField(
-        label='Title',
-        validators=[DataRequired(),
-                    Length(max=255, min=3, message='Title Allow 2-255 chars')],
-
-    )
-    customer = StringField(
-        label='Customer',
-        validators=[DataRequired(),
-                    Length(max=255, min=3, message='Customer Allow 2-255 chars')], )
-    location = StringField(
-        label='Location',
-        validators=[DataRequired(),
-                    Length(max=255, min=3, message='Location Allow 2-255 chars')
-                    ],
-    )
-    start_time = DateTimeField(
-        label='Date and Start Time',
-        validators=[DataRequired()],
-        format='%Y-%m-%d %H:%M'
-    )
-    duration = IntegerField(
-        label='Meeting Duration(is Minutes)',
-        validators=[DataRequired(),
-                    NumberRange(min=10, max=1440, message='Meeting Duration should be Integer and between 10-1440')])
-    note = TextAreaField(label='Notes')
-    is_completed = StringField(
-        label='Is Completed',
-        validators=[DataRequired()]
-
-                               )
+class SearchForm(FlaskForm):
+    search = StringField('Search')
     submit = SubmitField('submit')
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,15 +1,15 @@
-from flask import render_template,  redirect, url_for, request
+from flask import render_template,  redirect, url_for
 from app.main import bp
 from app import db
-from app.main.forms import TaskForm
+from app.main.forms import TaskForm, AppointmentForm, SearchForm
 from app.models import Task, Appointment
+from datetime import datetime
+from datetime import datetime as dt
 
-from app.main.forms import AppointmentForm, AppointmentCreationForm, AppointmentEditionForm
-from app.models import Appointment
-from datetime import timedelta, datetime
+
 
 # Main route of the applicaitons.
-@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET','POST'])
 def index():
     return render_template("main/index.html")
 
@@ -78,226 +78,112 @@ def edit_task(task_id):
     return render_template("main/todolist_edit_view.html",form=form, task_id = task_id)
 
 
-#  Route for viewing and adding new appointments.
-@bp.route('/appointment', methods=['GET','POST'])
-def appointment():
-    # form = AppointmentForm()
-    #
-    # if form.validate_on_submit():
-    #     # Get the data from the form, and add it to the database.
-    #     new_appointment = Appointment()
-    #     new_appointment.appointment_desc =  form.appointment_desc.data
-    #     new_appointment.appointment_status = form.appointment_status_completed.data
-    #     db.session.add(new_appointment)
-    #     db.session.commit()
-    #
-    #     # Redirect to this handler - but without form submitted - gets a clear form.
-    #     return redirect(url_for('main.appointment'))
-    #
-    #     #appointment_title = request.form.get("title")
-    #     #appointment_location = request.form.get("location")
-    #     #dt = request.form.get("date-time")
-    #     #ft = '%Y-%m-%dT%H:%M'
-    #     #result = datetime.strptime(dt, ft)
-    #
-    # new_appointment = db.session.query(Appointment).all()
-    #
-    # return render_template("main/appointment.html",new_appointment = new_appointment,form= form)
-    return redirect(url_for('main.app_list'))
-
 #
-# Route for removing a appointment
-@bp.route('/appointment/remove/<int:appointment_id>', methods=['GET','POST'])
-def remove_appointment(appointment_id):
+#  Route for viewing and adding new tasks.
+@bp.route('/appointmentlist/<int:sortby>/', methods=['GET','POST'])
+def appointmentlist(sortby):
+    form = AppointmentForm()
 
+    if form.validate_on_submit():
+        # Get the data from the form, and add it to the database.
+        new_appointment = Appointment(appointment_title=form.appointment_title.data,
+                                      appointment_start_date=form.appointment_start_date.data,
+                                      appointment_duration = form.appointment_duration.data,
+                                      appointment_location = form.appointment_location.data,
+                                      customer_name = form.customer_name.data,
+                                      customer_notes = form.customer_notes.data)
+
+        db.session.add(new_appointment)
+        db.session.commit()
+        # Redirect to this handler - but without form submitted - gets a clear form.
+        return redirect(url_for('main.appointmentlist', sortby=sortby))
+
+    if sortby==0:
+        appointment_list = Appointment.query.order_by(Appointment.appointment_title)
+    elif sortby==1:
+        appointment_list = Appointment.query.order_by(Appointment.appointment_start_date)
+    elif sortby==2:
+        appointment_list = Appointment.query.order_by(Appointment.appointment_duration)
+    elif sortby==3:
+        appointment_list = Appointment.query.order_by(Appointment.appointment_location)
+    elif sortby==4:
+        appointment_list = Appointment.query.order_by(Appointment.customer_name)
+    elif sortby==5:
+        appointment_list = Appointment.query.order_by(Appointment.customer_notes)
+    else:
+        appointment_list = db.session.query(Appointment).all()
+    return render_template("main/appointmentlist.html",appointment_list = appointment_list,form= form, sortby=sortby)
+
+
+
+@bp.route('/appointmentlist/remove/<int:appointment_id>', methods=['GET','POST'])
+def remove_appointment(appointment_id):
     # Query database, remove items
+    print(appointment_id)
     Appointment.query.filter(Appointment.appointment_id == appointment_id).delete()
     db.session.commit()
+    return redirect(url_for('main.appointmentlist', sortby=0))
 
-    return redirect(url_for('main.appointment'))
-
-# Route for editing a appointment
-
-# @bp.route('/appointment/edit/<int:appointment_id>', methods=['GET','POST'])
-# def edit_appointment(appointment_id):
-#     form = AppointmentForm()
-#     print(form.validate_on_submit())
-#     if form.validate_on_submit():
-#         # Get the data from the form, and add it to the database.
-#
-#         current_appointment = Appointment.query.filter_by(appointment_id=appointment_id).first_or_404()
-#         current_appointment.appointment_desc =  form.appointment_desc.data
-#         current_appointment.appointment_status = form.appointment_status_completed.data
-# #
-#         #current_appointment.appointment_title =  form.appointment_title.data
-#         #current_appointment.appointment_customername =  form.appointment_customername.data
-#         #current_appointment.appointment_location =  form.appointment_location.data
-#         #current_appointment.appointment_date =  form.appointment_date.data
-#
-#         db.session.add(current_appointment)
-#         db.session.commit()
-#         # After editing, redirect to the view page.
-#         return redirect(url_for('main.appointment'))
-#
-#     # get appointment for the database.
-#     current_appointment = Appointment.query.filter_by(appointment_id=appointment_id).first_or_404()
-#
-#     # update the form model in order to populate the html form.
-#     form.appointment_desc.data =     current_appointment.appointment_desc
-#     form.appointment_status_completed.data = current_appointment.appointment_status
-# #
-#     #form.appointment_title.data =     current_appointment.appointment_title
-#     #form.appointment_customername.data =     current_appointment.appointment_customername
-#     #form.appointment_location.data =     current_appointment.appointment_location
-#     #form.appointment_date.data =     current_appointment.appointment_date
-#
-#     return render_template("main/appointment_edit_view.html",form=form, appointment_id = appointment_id)
-
-
-@bp.route('/appointment/create', methods=['post', 'get'])
-def app_create():
-    print('hello')
-    form = AppointmentCreationForm()
+@bp.route('/appointment/<int:appointment_id>', methods=['GET','POST'])
+def appointment(appointment_id):
+    form = AppointmentForm()
     if form.validate_on_submit():
-        appointment = Appointment(
-            title=form.title.data,
-            customer=form.customer.data,
-            location=form.location.data,
-            start_time=form.start_time.data,
-            duration=form.duration.data,
-            note=form.note.data,
-        )
-        db.session.add(appointment)
+        # Get the data from the form, and add it to the database.
+        modified_appointment = Appointment(appointment_title=form.appointment_title.data,
+                                      appointment_start_date=form.appointment_start_date.data,
+                                      appointment_duration = form.appointment_duration.data,
+                                      appointment_location = form.appointment_location.data,
+                                      customer_name = form.customer_name.data,
+                                      customer_notes = form.customer_notes.data)
+        Appointment.query.filter(Appointment.appointment_id == appointment_id).delete()
         db.session.commit()
-        return redirect(url_for('main.app_list', _id=appointment.id))
-    return render_template('main/appointment_create.html', form=form, title='Appointment Creation')
+        db.session.add(modified_appointment)
+        db.session.commit()
+
+    appointment=Appointment.query.filter(Appointment.appointment_id == appointment_id).one()
+    return render_template("main/appointment.html",appointment = appointment,form= form, appointment_id=appointment_id)
+
+@bp.route('/appointmentsearch/<int:filterby>', methods=['GET','POST'])
+def search_appointment(filterby):
+    def sameDay(now,specificAppointmentTime):
+        #print(specificAppointmentTime.year, now.year, specificAppointmentTime.month,now.month, specificAppointmentTime.day,now.day)
+        return specificAppointmentTime.year==now.year and specificAppointmentTime.month==now.month and specificAppointmentTime.day==now.day
+    def sameWeek(now,specificAppointmentTime):
+        #print(now.isocalendar()[1], specificAppointmentTime.isocalendar()[1], now.year, specificAppointmentTime.year)
+        return now.isocalendar()[1]==specificAppointmentTime.isocalendar()[1] and now.year==specificAppointmentTime.year
+
+    appointment_list = Appointment.query.all()
+    now = datetime.now()
 
 
-@bp.route('/appointment/edit/<int:_id>', methods=['post', 'get'])
-def app_edit(_id):
-    a = Appointment.query.get_or_404(_id)
-    form = AppointmentEditionForm(
-        title=a.title,
-        customer=a.customer,
-        location=a.location,
-        start_time=a.start_time,
-        duration=a.duration,
-        note=a.note,
-        is_completed=a.is_completed
+    if filterby==0:#No Filter
+        print('No Filter')
+    elif filterby==1:#Filter for today
+        for appointment in reversed(appointment_list):#if not reversed, array will skip certian items when removed
+            if not sameDay(now, appointment.appointment_start_date):
+                appointment_list.remove(appointment)
+    elif filterby==2:#Filter for this week
+        for appointment in reversed(appointment_list):#if not reversed, array will skip certian items when removed
+            if not sameWeek(now, appointment.appointment_start_date):
+                appointment_list.remove(appointment)
+    elif filterby==3:#Filter Overdue (Everything before current datetime)
+        for appointment in reversed(appointment_list):#if not reversed, array will skip certian items when removed
+            if appointment.appointment_start_date>now:
+                appointment_list.remove(appointment)
+    else:
+        print('No Filter')
 
-    )
-
+    #filters based on what user is searching for
+    search=''
+    form = SearchForm()
     if form.validate_on_submit():
+        search=form.search.data
+        if search != '':
+            for appointment in reversed(appointment_list):
+                if search in appointment.customer_name or search in appointment.appointment_title:
+                    print(search, 'found')
+                else:
+                    appointment_list.remove(appointment)
 
-        a.title = form.title.data
-        a.customer = form.customer.data
-        a.location = form.location.data
-        a.start_time = form.start_time.data
-        a.duration = form.duration.data
-        a.note = form.note.data
-        a.is_completed = form.is_completed.data
-
-        db.session.add(a)
-        db.session.commit()
-        return redirect(url_for('main.app_view', _id=a.id))
-    print(request.method, 'a.is_completed', a.is_completed, form.is_completed.data)
-    return render_template('main/appointment_edit.html', form=form, title='Appointment Edit')
-
-
-@bp.route('/appointment/delete/<int:_id>')
-def app_delete(_id):
-    a = Appointment.query.get_or_404(_id)
-    db.session.delete(a)
-    db.session.commit()
-    return redirect(url_for('main.app_list'))
-
-
-@bp.route('/appointment/list')
-def app_list():
-    query = Appointment.query.order_by(Appointment.id.desc())
-    title = request.args.get('title')
-    if title:
-        query = query.filter(Appointment.title.like('%{}%'.format(title)))
-    customer = request.args.get('customer')
-    if customer:
-        query = query.filter(Appointment.customer.like('%{}%'.format(customer)))
-    location = request.args.get('location')
-    if location:
-        query = query.filter(Appointment.location.like('%{}%'.format(location)))
-    note = request.args.get('note')
-    if note:
-        query = query.filter(Appointment.note.like('%{}%'.format(note)))
-    begin = request.args.get('begin')
-    try:
-        datetime.strptime(begin, '%Y-%m-%d %H:%M')
-    except (ValueError, TypeError):
-        begin = None
-
-    if begin:
-        print(query.count())
-        print('query begin', begin)
-        query = query.filter(Appointment.start_time >= begin)
-        print(query.count())
-
-    end = request.args.get('end')
-    try:
-        datetime.strptime(end, '%Y-%m-%d %H:%M')
-    except (ValueError, TypeError):
-        end = None
-    if end:
-        query = query.filter(Appointment.start_time <= end)
-
-    is_completed = request.args.get('is_completed')
-    if is_completed not in ['Not Completed', 'Completed']:
-        is_completed = None
-    if is_completed:
-        print('query is_completed', is_completed)
-        query = query.filter(Appointment.is_completed == is_completed)
-
-    page = request.args.get('page', 1)
-    try:
-        page = int(page)
-    except (TypeError, ValueError):
-        page = 1
-    if page <= 0:
-        page = 1
-    per_page = request.args.get('per_page')
-    try:
-        per_page = int(per_page)
-    except (TypeError, ValueError):
-        per_page = 20
-    if isinstance(per_page, int) and per_page >= 5:
-        pass
-    else:
-        per_page = 20
-    is_overdue = request.args.get('is_overdue')
-    if is_overdue == 'Overdue':
-        max_per_page = query.count()
-    elif is_overdue == 'Not Overdue':
-        max_per_page = query.count()
-    else:
-        is_overdue = None
-        max_per_page = 100
-    if max_per_page < 100:
-        max_per_page = 100
-
-    ls = query.paginate(
-        page=page, per_page=per_page, max_per_page=max_per_page, error_out=False
-    )
-    print(locals())
-    return render_template('main/appointment_list.html', ls=ls, is_overdue=is_overdue)
-
-
-@bp.route('/appointment/view/<int:_id>', methods=['get'])
-def app_view(_id):
-    a = Appointment.query.get_or_404(_id)
-    form = AppointmentEditionForm(
-        title=a.title,
-        customer=a.customer,
-        location=a.location,
-        start_time=a.start_time,
-        duration=a.duration,
-        note=a.note,
-        is_completed=a.is_completed
-    )
-    return render_template('main/appointment_view.html', form=form, title='Appointment View', a=a)
+    count = len(appointment_list)
+    return render_template("main/appointmentsearch.html",appointment_list = appointment_list, filterby=filterby,search=search, count=count, form=form)
